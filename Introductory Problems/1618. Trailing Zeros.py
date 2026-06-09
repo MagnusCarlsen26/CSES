68aa08064a41c5c2dcd9967e5f69665c199fab64
MagnusCarlsen26 <khushal.sindhav26@gmail.com>
2026-06-09 20:40:00 +0530

    feat: 1618. Trailing Zeros

--- Introductory Problems/1618. Trailing Zeros.py
+++ Introductory Problems/1618. Trailing Zeros.py
@@ -1,1 +1,21 @@
 # 1618. Trailing Zeros
+
+def countFactorOfFive(n: int) -> int:
+    
+    c = 0
+    while( n%5 == 0):
+        n = n//5
+        c += 1
+
+    return c
+
+def main():
+    n = int(input())
+    
+    numberOfZeroesInNFactorial = 0
+    for i in range(1, n+1):
+        numberOfZeroesInNFactorial += countFactorOfFive(i)
+
+    print(numberOfZeroesInNFactorial)
+
+main()
