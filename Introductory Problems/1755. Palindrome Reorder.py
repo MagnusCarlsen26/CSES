8958eca943b07d716ede7e9e925cf2155327fc1a
MagnusCarlsen26 <khushal.sindhav26@gmail.com>
2026-06-11 10:07:40 +0530

    feat: 1755. Palindrome Reorder

--- Introductory Problems/1755. Palindrome Reorder.py
+++ Introductory Problems/1755. Palindrome Reorder.py
@@ -1,1 +1,34 @@
 # 1755. Palindrome Reorder
+
+def solve(s: str) -> str:
+    
+    d = {}
+    for i in s:
+        if i in d:
+            d[i] +=1
+        else:
+            d[i] = 1
+
+    isOddFreqFound = False
+    oddFreqCharacter = ""
+    for i in d:
+        if d[i]%2 != 0:
+            if isOddFreqFound:
+                return "NO SOLUTION"
+            isOddFreqFound = True
+            oddFreqCharacter = i
+
+    finalAnswer = []
+    for i in d:
+        finalAnswer.append(i*(d[i]//2))
+
+    strFinalAnswer = "".join(finalAnswer)
+    strFinalAnswer = strFinalAnswer + oddFreqCharacter + strFinalAnswer[::-1]
+
+    return strFinalAnswer
+
+def main():
+    s = input()
+    print(solve(s))
+
+main()
