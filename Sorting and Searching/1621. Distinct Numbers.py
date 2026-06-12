8afb11ff30c3846aa1a3d1fcb981edb9195eefad
MagnusCarlsen26 <khushal.sindhav26@gmail.com>
2026-06-12 15:53:03 +0530

    feat: 1621. Distinct Numbers

--- Sorting and Searching/1621. Distinct Numbers.py
+++ Sorting and Searching/1621. Distinct Numbers.py
@@ -1,1 +1,16 @@
 # 1621. Distinct Numbers
+
+def solve(n: int, lst: list) -> int:
+    return len(set(lst))
+def main():
+    n = int(input())
+    lst = list(map(int,input().split()))
+    print(solve(n, lst))
+
+main()
+
+# Analysis
+# https://codeforces.com/blog/entry/122914
+# Why the fuck same problem when solved using dict takes 3s+ to run. TLE 
+# For memory constraint issues sort the array. Space O(logn) Time O(nlogn)
+# For spare 
