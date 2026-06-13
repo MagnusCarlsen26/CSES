c6479bb6c043e580eab935d6b727e5f1b9af78ce
MagnusCarlsen26 <khushal.sindhav26@gmail.com>
2026-06-13 09:41:50 +0530

    feat: 1090. Ferris Wheel

--- Sorting and Searching/1090. Ferris Wheel.py
+++ Sorting and Searching/1090. Ferris Wheel.py
@@ -1,1 +1,19 @@
 # 1090. Ferris Wheel
+n, x = map(int, input().split())
+
+lst = list(map(int, input().split()))
+
+lst.sort()
+
+gondalasUsed = 0
+left, right = 0, n-1
+while(left<right):
+    if lst[left] + lst[right] <=x:
+        left += 1
+        right -= 1
+    else:
+        right -= 1
+    gondalasUsed += 1
+        
+
+print(gondalasUsed + int(left == right))
