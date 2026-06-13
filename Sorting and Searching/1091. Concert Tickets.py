1faadf1177db6d0573ed7d4b71a6d88e60b58449
MagnusCarlsen26 <khushal.sindhav26@gmail.com>
2026-06-13 09:41:50 +0530

    feat: 1091. Concert Tickets

--- Sorting and Searching/1091. Concert Tickets.py
+++ Sorting and Searching/1091. Concert Tickets.py
@@ -1,1 +1,34 @@
 # 1091. Concert Tickets
+
+n, m = map(int, input().split())
+concertPrices = list(map(int, input().split()))
+customerBidPrices = list(map(int, input().split()))
+
+concertPrices.sort()
+customerBidPrices.sort(reverse=True)
+
+concertPricesLastAvailableIndex = n-1
+
+for bidPrice in range(m):
+    
+    if concertPricesLastAvailableIndex < 0:
+        print(-1)
+        continue
+
+    start, end = 0, concertPricesLastAvailableIndex
+    ans = -1
+    while(start < end):
+        mid = start + (end - start)//2
+        if concertPrices[mid] == customerBidPrices[bidPrice]: 
+            print(concertPrices[mid])
+            break
+        elif concertPrices[mid] > customerBidPrices[bidPrice]:
+            concertPricesLastAvailableIndex = mid - 1
+            end = mid - 1
+        elif concertPrices[mid] < customerBidPrices[bidPrice]:
+            ans = concertPrices[mid]
+            start = mid + 1
+        else:
+            exit()
+    
+    print(ans)
