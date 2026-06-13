b557e90752dda18ef2805eb05187f24013137491
MagnusCarlsen26 <khushal.sindhav26@gmail.com>
2026-06-13 09:46:12 +0530

    feat: 1091. Concert Tickets

--- Sorting and Searching/1091. Concert Tickets.py
+++ Sorting and Searching/1091. Concert Tickets.py
@@ -1,34 +1,32 @@
-# 1091. Concert Tickets
+#include <bits/stdc++.h>
 
-n, m = map(int, input().split())
-concertPrices = list(map(int, input().split()))
-customerBidPrices = list(map(int, input().split()))
+using namespace std;
 
-concertPrices.sort()
-customerBidPrices.sort(reverse=True)
+// variables used for the current problem
+int n, m, h, t;
+multiset<int> tickets;
 
-concertPricesLastAvailableIndex = n-1
+void solve() {
+	cin >> n >> m;
+	for (int i = 0; i < n; ++i) {
+		cin >> h;
+		tickets.insert(h);
+	}
+	for (int i = 0; i < m; ++i) {
+		cin >> t;
+		auto it = tickets.upper_bound(t);
+		if (it == tickets.begin()) {
+			cout << -1 << "\n";
+		} else {
+			cout << *(--it) << "\n";
+			tickets.erase(it);
+		}
+	}
+}
 
-for bidPrice in range(m):
-    
-    if concertPricesLastAvailableIndex < 0:
-        print(-1)
-        continue
-
-    start, end = 0, concertPricesLastAvailableIndex
-    ans = -1
-    while(start < end):
-        mid = start + (end - start)//2
-        if concertPrices[mid] == customerBidPrices[bidPrice]: 
-            print(concertPrices[mid])
-            break
-        elif concertPrices[mid] > customerBidPrices[bidPrice]:
-            concertPricesLastAvailableIndex = mid - 1
-            end = mid - 1
-        elif concertPrices[mid] < customerBidPrices[bidPrice]:
-            ans = concertPrices[mid]
-            start = mid + 1
-        else:
-            exit()
-    
-    print(ans)
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	solve();
+	return 0;
+}
