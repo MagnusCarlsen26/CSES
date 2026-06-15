ccdc3b55e2ab8801e51182b41757bb6c384852f5
MagnusCarlsen26 <khushal.sindhav26@gmail.com>
2026-06-15 09:03:59 +0530

    feat: 1629. Movie Festival

--- Sorting and Searching/1629. Movie Festival.py
+++ Sorting and Searching/1629. Movie Festival.py
@@ -1,1 +1,28 @@
 # 1629. Movie Festival
+
+def maxMovieWatchingPossible(n: int, movieTimings: list) -> int:
+
+    movieTimings.sort(key=lambda x:x[1])
+
+    count = 0
+    lastMovieEndTime = -1
+    for movieTime in movieTimings:
+        if movieTime[0] >= lastMovieEndTime:
+            count += 1
+            lastMovieEndTime = movieTime[1]
+
+    return(count) 
+
+def main():
+    n = int(input())
+    movieTimings = []
+    for _ in range(n):
+        movieStartTime, movieEndTime = map(int, input().split())
+        movieTimings.append((
+            movieStartTime,
+            movieEndTime
+        ))
+
+    print(maxMovieWatchingPossible(n, movieTimings))
+
+main()
