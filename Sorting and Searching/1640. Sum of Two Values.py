652ede48acae7f81c490c274ef2764e16cf084e8
MagnusCarlsen26 <khushal.sindhav26@gmail.com>
2026-06-15 09:03:59 +0530

    feat: 1640. Sum of Two Values

--- Sorting and Searching/1640. Sum of Two Values.py
+++ Sorting and Searching/1640. Sum of Two Values.py
@@ -1,1 +1,32 @@
 # 1640. Sum of Two Values
+import sys
+
+input = sys.stdin.readline
+
+n, x = map(int, input().split())
+lst = list(map(int, input().split()))
+
+d = {}
+s = set()
+for i in range(n):
+    if lst[i] not in s:
+        s.add(lst[i])
+        d[lst[i]] = [i]
+    else:
+        if len(d[lst[i]]) > 2:
+            continue
+        d[lst[i]].append(i)
+
+for i in lst:
+    if x - i in d:
+        if x-i != i:
+            print(d[i][0] + 1, d[x-i][0] + 1)
+            break
+        elif x-i == i:
+            if len(d[i]) != 1:
+                print(d[i][0] + 1, d[i][1] + 1)
+                break
+        else:
+            exit()
+else:
+    print("IMPOSSIBLE")
